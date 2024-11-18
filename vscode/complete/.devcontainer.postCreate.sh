b1a85e38eba2db16cb54f8f27b4047f5f3046efa
nobody <nobody@nttdata.com>
2024-11-18 18:42:02 +0000

    adding containeruser

--- vscode/complete/.devcontainer.postCreate.sh
+++ vscode/complete/.devcontainer.postCreate.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 # This script is ran within the dev container and does not make
 # changes to the host machine. It is used to set up the dev container.
+# after it has built.
 #
-# @param container_user: The user that the dev container is running as
 # @param work_dir: The vscode workspace open in the dev container
 # @param git_token: The GitHub token used to authenticate with GitHub
 # @param sso_aws_url: The URL for the AWS SSO login page
@@ -14,9 +14,13 @@
 # @param github_public_user: The GitHub username to use for public repositories
 # @param github_public_email: The email address to use for public repositories
 
-work_dir="/workspaces/CHANGEME"
+# Work directory of the mounted workspace
+work_dir="/workspaces/workplace"
+
+# Tokens
 git_token="CHANGEME"
 
+# AWS params
 sso_aws_url="CHANGEME"
 sso_aws_region="us-east-1"
 
@@ -25,67 +29,46 @@ aws_prod_account_id="CHANGEME"
 aws_sandbox_account_id="CHANGEME"
 aws_region="us-east-2"
 
+# Github params
 github_public_user="CHANGEME"
 github_public_email="CHANGEME@CHANGEME.com"
 
-# Set your ENV vars
+#################
+# Configure ENV #
+#################
+container_user=$1
 bash_rc="/home/${container_user}/.bashrc"
 zsh_rc="/home/${container_user}/.zshrc"
+export GITHUB_TOKEN=${git_token}
+
+# Set your ENV vars
+echo "Configuring ENV vars..."
 echo 'export GITHUB_TOKEN='${git_token} | tee -a ${bash_rc} ${zsh_rc}
 echo 'export GOPRIVATE="github.com/launchbynttdata"' | tee -a ${bash_rc} ${zsh_rc}
-
-# Local user scripts to added to PATH for execution
 echo 'export PATH="'${work_dir}'/.localscripts:${PATH}"' | tee -a ${bash_rc} ${zsh_rc}
 
-# Install repo
-mkdir -p /home/${container_user}/.bin
-echo 'export PATH="${HOME}/.bin/repo:${PATH}"' | tee -a ${bash_rc} ${zsh_rc}
-git clone https://github.com/launchbynttdata/git-repo.git /home/${container_user}/.bin/repo
-chmod a+rx /home/${container_user}/.bin/repo
-
-# Install asdf
-git clone https://github.com/asdf-vm/asdf.git /home/${container_user}/.asdf --branch v0.14.0
-echo '. "$HOME/.asdf/asdf.sh"' | tee -a ${bash_rc} ${zsh_rc}
-
-# Install dependencies
-python -m pip install aws-sso-util
-python -m pip install ruamel_yaml
-
-# Install Launch CLI as a dev dependency
-python -m pip install launch-cli
-
-## Uncomment the following lines if wish to have a local 
-## dev source as the install folder
-# cd ${work_dir}/launch-cli
-# python -m pip install -e '.[dev]'
-# python3.12 -m pip install boto3
-# cd ${work_dir}
-
-# Set up netrc
-echo "Setting /home/${container_user}/.netrc variables"
-echo "machine github.com
-login ${github_public_user}
-password ${git_token}" >> /home/${container_user}/.netrc
-chmod 600 /home/${container_user}/.netrc
-
-# Configure git
-echo "
-[user]
-        name = ${github_public_user}
-        email = ${github_public_email}
-[push]
-        autoSetupRemote = true
-[safe]
-        directory = *
-" >> /home/${container_user}/.gitconfig
-
 # shell aliases
+echo "Configuring shell aliases..."
 echo 'alias git_sync="git pull origin main"' | tee -a ${bash_rc} ${zsh_rc} # Alias to sync the repo with the main branch
 echo 'alias git_boop="git reset --soft HEAD~1"' | tee -a ${bash_rc} ${zsh_rc} # Alias to undo the last local commit but keep the changes
 
-# Set up AWS Config
-# Default profile is set to the Sandbox Account.
-mkdir -p /home/${container_user}/.aws
+# Install Oh My Zsh
+echo "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="obraun"
+ENABLE_CORRECTION="false"
+HIST_STAMPS="%m/%d/%Y - %H:%M:%S"
+source $ZSH/oh-my-zsh.sh
+' | tee -a ${zsh_rc}
+
+#################
+# Configure aws #
+#################
+
+# Default profile is set to the Launch Sandbox Account.
+echo "Configuring .aws/config..."
+mkdir -p ~/.aws
 echo "[default]
 region = ${aws_region}
 output = json
@@ -126,16 +109,64 @@ sso_role_name = AdministratorAccess
 region = ${aws_region}
 credential_process = aws-sso-util credential-process --profile launch-sandbox-admin
 sso_auto_populated = true
-" >> /home/${container_user}/.aws/config
+" > /home/${container_user}/.aws/config
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo 'export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="obraun"
-ENABLE_CORRECTION="false"
-HIST_STAMPS="%m/%d/%Y - %H:%M:%S"
-source $ZSH/oh-my-zsh.sh
-' >> ${zsh_rc}
+#################
+# Install Tools #
+#################
+
+# Install repo
+mkdir -p /home/${container_user}/.bin
+echo 'export PATH="${HOME}/.bin/repo:${PATH}"' | tee -a ${bash_rc} ${zsh_rc}
+git clone https://github.com/launchbynttdata/git-repo.git /home/${container_user}/.bin/repo
+chmod a+rx /home/${container_user}/.bin/repo
+
+# Install asdf
+echo "Installing asdf tool..."
+git clone https://github.com/asdf-vm/asdf.git /home/${container_user}/.asdf --branch v0.14.0
+echo '. "$HOME/.asdf/asdf.sh"' | tee -a ${bash_rc} ${zsh_rc}
+
+# Install aws-sso-util as a make check dependency
+python -m pip install aws-sso-util
+
+# Install Launch CLI as a dev dependency
+python -m pip install launch-cli
+python -m pip install ruamel_yaml
+
+## Uncomment the following lines if wish to have a local 
+## dev source as the install folder
+# cd ${work_dir}/launch-cli
+# python -m pip install -e '.[dev]'
+# python3.12 -m pip install boto3
+# cd ${work_dir}container_user
+
+#################
+# Configure git #
+#################
+
+# Set up netrc
+echo "Setting ~/.netrc variables..."
+echo machine github.com >> ~/.netrc
+echo login ${github_public_user} >> ~/.netrc
+echo password ${git_token} >> ~/.netrc
+chmod 600 /home/${container_user}/.netrc
+
+# Configure git
+echo "Configuring git..."
+echo "[user]
+        name = ${github_public_user}
+        email = ${github_public_email}
+[credential]
+        credentialStore = cache
+[push]
+        autoSetupRemote = true
+[safe]
+        directory = *
+" >> /home/${container_user}/.gitconfig
+
+###########
+# Cleanup #
+###########
 
 # Set folder permissions
 chown -R ${container_user}:${container_user} /home/${container_user}
